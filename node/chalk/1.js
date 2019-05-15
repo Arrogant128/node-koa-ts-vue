87d9a3b9f4b9a113b0d4be2df4d1b9ec43c2cdf1
ysj <978614237@qq.com>
2019-05-15 09:19:09 +0800

    添加忽略文件

--- node/chalk/1.js
+++ node/chalk/1.js
@@ -2,11 +2,12 @@
  * @Author: yu shijun
  * @Date: 2019-05-12 14:30:39
  * @LastEditors: yu shijun
- * @LastEditTime: 2019-05-12 14:44:24
+ * @LastEditTime: 2019-05-15 09:17:50
  * @Description: 
  */
 const chalk = require('chalk')
 const commander = require('commander')
+const express = require('express')
 
 commander.version('v1.o.o.o', '-v, --version')
 console.log(chalk.bold.red('hello world!') + 'miaov')
